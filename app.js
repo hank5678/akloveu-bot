d83d9b12c54e32c8bb40807a6cc982b5693e018d
MasterKuei <hank5678tw@gmail.com>
2022-01-24 19:06:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-// var token = "5231899144:AAFiLdn3dA54HXUOiGsoI36jx6WO0I1ZGYI"
-var token = "5026027683:AAGsSpHsUATaee2ld6vda5kNixNg_tDE6FQ"
+var token = "5231899144:AAFiLdn3dA54HXUOiGsoI36jx6WO0I1ZGYI"
+// var token = "5026027683:AAGsSpHsUATaee2ld6vda5kNixNg_tDE6FQ"
 //括號裡面的內容需要改為在第5步獲得的Token
 var bot = new TelegramBot(token, { polling: true })
 //使用Long Polling的方式與Telegram伺服器建立連線
 
-const chatIds = [591309041] // 456371558 591309041
+const chatIds = [456371558] // 456371558 591309041
 
 //收到Start訊息時會觸發這段程式
 // bot.onText(/\/start/, function (msg) {
